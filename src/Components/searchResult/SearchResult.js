b50b263757ddd5affe0796112278da95c3dedf98
Steven <sfcastroverde@iMac.PK5001Z>
2017-06-14 16:38:55 -0600

    added click handler to selected movie

--- src/Components/searchResult/SearchResult.js
+++ src/Components/searchResult/SearchResult.js
@@ -3,9 +3,15 @@ import './searchResult.css';
 
 
 class SearchResult extends Component{
+    selectMovie(e){
+        e.preventDefault();
+        console.log(this.props.movie.id)
+    }
+
+
     render() {
         return (
-            <div className="result-card">
+            <div onClick={(e) =>this.selectMovie(e)} className="result-card">
                 <img className="poster" src={this.props.movie.poster_240x342}/>
                 <div className="caption">
                     <h3 className="truncate title">{this.props.movie.title}</h3>
