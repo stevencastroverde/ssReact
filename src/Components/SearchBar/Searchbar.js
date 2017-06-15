bd7094b8e354b42eb281f9aababfe9e9fe95f30f
Steven <sfcastroverde@iMac.PK5001Z>
2017-06-14 22:44:23 -0600

    working on submit values of checkboxes

--- src/Components/SearchBar/Searchbar.js
+++ src/Components/SearchBar/Searchbar.js
@@ -2,18 +2,35 @@ import React, { Component } from 'react';
 import './Searchbar.css';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.submitHandler = this.submitHandler.bind(this);
+    }
+    toggleCheckbox(label) {
+        console.log(e)
+    }
+
+
+    submitHandler(e){
+        e.preventDefault();
+        console.log(e.target.value)
+    }
+
+
     render() {
         return (
-            <form>
-                <input type="text" placeholder="Search"/>
-                <input type="checkbox" id="amazon-prime" value="amazon_prime"/>
+            <form onSubmit={this.submitHandler}>
+                <input type="text" placeholder="Search" value={this.state.value} />
+                <input type="checkbox" id="amazon-prime" value="amazon_prime" onChange={this.toggleCheckbox}/>
                 <label for="amazon-prime">Amazon Prime</label>
                 <input type="checkbox" id="netflix" value="netflix"/>
                 <label for="netflix">Netflix</label>
                 <input type="checkbox" id="hulu" value="hulu_plus"/>
                 <label for="hulu">Hulu</label>
-                <input type="checkbox" id="crunchyroll" value="crunchroll_premium"/>
-                <label for="crunchroll">Crunchroll</label>
+                <input type="checkbox" id="crunchyroll" value="crunchyroll_premium"/>
+                <label for="crunchyroll">Crunchyroll</label>
                 <input type="checkbox" id="drama-fever" value="dramafever_premium"/>
                 <label for="drama-fever">Drama Fever</label>
                 <input type="checkbox" id="hbo" value="hbo_now"/>
