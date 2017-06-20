72fbc939f7be5aa4c6fd3536a01127fde09d0cc9
Steven <sfcastroverde@iMac.PK5001Z>
2017-06-19 21:10:28 -0600

    added trailer to movie page

---
+++ src/Components/Movie/MovieInfo/MovieInfo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './MovieInfo.css';
+
+
+
+
+const MovieInfo = props => {
+    const inTheaters = props.inTheaters ?  'Currently in Theaters' : 'Not In Theaters';
+    const moreThanOneDirector = props.directors.length > 1 ? 'Directors' : 'Director';
+    return (
+        <div className="movie-info-card">
+            <h5>{props.title}</h5>
+            <h5>{props.year}</h5>
+            <h5>{inTheaters}</h5>
+            <h5>{moreThanOneDirector} :</h5>
+            {props.directors.map(person => {
+                return (
+                        <h5  key={person.id} >{person.name}</h5>
+                )
+            } )}
+
+        </div>
+    )
+}
+
+export default MovieInfo
