b9b1838416753ca83897315747cc94f986ca700a
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-06-29 13:48:50 -0600

    finished testing <SearchBar/>

--- src/Components/Search/SearchBar/SearchBar.js
+++ src/Components/Search/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SubscriptionSources from '../../../data/SubscriptionSources.js';
 import './SearchBar.css';
 
@@ -6,7 +7,7 @@ const SearchBar = (props) => {
         return (
             <form onSubmit={props.submitSearch} className="search-bar">
                 <div className="radio-buttons">
-                    <input  type="radio" id="tv-radio-btn" required name="search-type" value='tv' onChange={props.radioCheck} />
+                    <input type="radio" id="tv-radio-btn" required name="search-type" value='tv' onChange={props.radioCheck} />
                     <label htmlFor="tv-radio-btn">Television</label>
                     <input type="radio" id="movie-radio-btn" name="search-type" value='movies'  onChange={props.radioCheck} />
                     <label htmlFor="movie-radio-btn">Movies</label>
@@ -24,14 +25,19 @@ const SearchBar = (props) => {
 								})}
                         </div>
                 <div className="search-buttons">
-                    <button type="submit">Search</button>
+                    <button id="submitButton" type="submit">Search</button>
                     <button type="reset">Clear</button>
                 </div>
             </form>
         )
 
-}
+};
 
+SearchBar.propTypes = {
+    submitSearch: PropTypes.func,
+    radioCheck: PropTypes.func,
+    inputChange: PropTypes.func
+};
 
 
 export default SearchBar
--- src/Components/Search/SearchBar/SearchBar.test.js
+++ src/Components/Search/SearchBar/SearchBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import SearchBar from './SearchBar';
 
 describe('<SearchBar /> UI Component', () => {
@@ -14,11 +14,57 @@ describe('<SearchBar /> UI Component', () => {
 
     it('invokes onSubmit', () => {
         const _submit = jest.fn();
-        shallow(<SearchBar onSubmit={_submit}/>)
-            .find('[type="submit"]')
+        mount(<SearchBar submitSearch={_submit}/>)
+            .find('#submitButton')
             .simulate('submit');
-
             expect(_submit).toBeCalled()
+    });
 
+    it('calls function on checkbox clicked', () => {
+        const _checkbox = jest.fn();
+        const checkTest = {
+            type:'checkbox',
+            name:'test check',
+            checked: true,
+            value: 'poop'
+        };
+        mount(<SearchBar inputChange={_checkbox}/>)
+            .find('.checkbox').first()
+            .simulate('change', {target: checkTest});
+        expect(_checkbox).toBeCalled()
+    });
+
+    it('calls changeFunction for text input change', () => {
+        const _typing = jest.fn();
+        mount(<SearchBar inputChange={_typing}/>)
+            .find('#search-input')
+            .simulate('change');
+        expect(_typing).toHaveBeenCalled();
+    });
+    it('calls changeFunction multiple times', () => {
+       const _multiTyping = jest.fn();
+        const wrapper = mount(<SearchBar inputChange={_multiTyping}/>);
+        const searchInput = wrapper.find('#search-input');
+        searchInput.simulate('change');
+        searchInput.simulate('change');
+        searchInput.simulate('change');
+        expect(_multiTyping).toHaveBeenCalledTimes(3);
+    });
+    it('calls function on radio button change', () => {
+        const _radioChange = jest.fn();
+        mount(<SearchBar radioCheck={_radioChange}/>)
+            .find('input[type="radio"]').first()
+            .simulate('change');
+        expect(_radioChange).toHaveBeenCalled();
+    });
+    it('changes search type on radio change', () => {
+        const _change = jest.fn();
+        const wrapper = mount(<SearchBar radioCheck={_change}/>);
+        const tvRadio = wrapper.find('#tv-radio-btn');
+        const movieRadio = wrapper.find('#movie-radio-btn');
+        tvRadio.simulate('change');
+        movieRadio.simulate('change');
+        tvRadio.simulate('change');
+        expect(_change).toHaveBeenCalledTimes(3);
     })
 });
