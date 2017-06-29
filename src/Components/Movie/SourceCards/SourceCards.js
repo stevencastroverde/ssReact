aced1a907f80c1ac953af0c01af312dac3d1f30e
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-06-29 14:35:52 -0600

    added tests for <SourceCards />

--- src/Components/Movie/SourceCards/SourceCards.js
+++ src/Components/Movie/SourceCards/SourceCards.js
@@ -8,19 +8,12 @@ import Source from './Source/Source';
 const SourceCards = props => {
     const userSubscriptions = props.subscriptionSources.length === 0 ?
         <h4>The Subscriptions you have don't stream this movie</h4>  :
-        props.subscriptionSources.map((source, i) => <Source key={'subscription ' + i} subscription={source}/>)
+        props.subscriptionSources.map((source, i) => <Source key={'subscription ' + i} subscription={source}/>);
     const freeContent = props.freeSources.length === 0 ?
         <h4>Sorry we couldn't find anywhere where you can watch this movie for free</h4> :
-        props.freeSources.map((source, i) => <Source key={'free ' + i} free={source}/>)
-    const purchaseContent = props.rentBuySources.map((source, i) => <Source key={'rent/buy' + i} rentBuy={source}/>)
+        props.freeSources.map((source, i) => <Source key={'free ' + i} free={source}/>);
+    const purchaseContent = props.rentBuySources.map((source, i) => <Source key={'rent/buy' + i} rentBuy={source}/>);
 
-    
-    
-    
-    
-    
-    
-    
     return (
         <section className="movie-sources">
             <h3>Subscription Sources:</h3>
--- src/Components/Movie/SourceCards/SourceCards.test.js
+++ src/Components/Movie/SourceCards/SourceCards.test.js
@@ -3,7 +3,16 @@ import { shallow } from 'enzyme';
 import SourceCards from './SourceCards';
 
 describe('<SourceCards /> UI Component', () => {
-    it('renders without crashing', () => {
-        shallow(<SourceCards/>);
+    const testArray = [{
+        name:'test'
+    }];
+    it('renders rent/buy source', () => {
+        shallow(<SourceCards subscriptionSources={[]} freeSources={[]} rentBuySources={testArray}/>)
     });
+    it('renders subscription sources', () => {
+        shallow(<SourceCards rentBuySources={[]} subscriptionSources={testArray} freeSources={[]}/>)
+    });
+    it('renders free sources', () => {
+        shallow(<SourceCards rentBuySources={[]} subscriptionSources={[]} freeSources={testArray}/>)
+    })
 });
